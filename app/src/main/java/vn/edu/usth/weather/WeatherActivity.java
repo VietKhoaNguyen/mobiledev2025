a884753482a9697de93b46e3fe1e67c7e5b5e723
Nguyen Viet Khoa <nguyenvietkhoa>
2025-09-16 21:10:53 +0700

    Upload USTHWeather project

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,53 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        if (savedInstanceState == null) {
+            ForecastFragment firstFragment = new ForecastFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, firstFragment)
+                    .commit();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+}
